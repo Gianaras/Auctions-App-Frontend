3505cd76ac8453a7abdd3d80f0f0bc52ec41b898
Anargyros Argyros <sdi1900014@di.uoa.gr>
2022-06-09 15:24:29 +0300

    message front

---
+++ src/app/messages/messages/message.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from "@angular/core";
+import {RouterModule} from "@angular/router";
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessageComponent implements OnInit{
+  ngOnInit(): void{
+  }
+}
---
+++ src/app/model/message.ts
@@ -0,0 +1,11 @@
+export class Message{
+  content: string;
+  sender:string;
+  receiver:string;
+
+  constructor(content:string,sender:string,receiver:string) {
+    this.content=content;
+    this.sender=sender;
+    this.receiver=receiver;
+  }
+}
