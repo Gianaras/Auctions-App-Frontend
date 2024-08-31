3963d62247a85f6810fc7ddd88cbeb390151a28d
Gianaras <Giannismav120@gmail.com>
2024-08-31 18:16:10 +0300

    redo css, javascript particles

---
+++ src/assets/particles.js
@@ -0,0 +1,64 @@
+const canvas = document.getElementById('particleCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+const particlesArray = [];
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = ((Math.random() * 2) - 1) * 0.25 * (this.size * 0.2);
+    this.speedY = ((Math.random() * 2) - 1) * 0.25 * (this.size * 0.2);
+    this.color = `rgba(255, 255, 255, ${0.1 * this.size * 0.25})`;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (this.x > canvas.width || this.x < 0) {
+      this.speedX = -this.speedX;
+    }
+    if (this.y > canvas.height || this.y < 0) {
+      this.speedY = -this.speedY;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+}
+
+function init() {
+  particlesArray.length = 0;
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  particlesArray.forEach(particle => {
+    particle.update();
+    particle.draw();
+  });
+
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
